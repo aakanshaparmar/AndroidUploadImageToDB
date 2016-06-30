81ec70cb7674d0bb1ed4fd3415c298c6e435751e
aakanshaparmar <parmar.aakansha@gmail.com>
2016-06-30 19:23:42 +0530

    Taking Picture and Displaying Done

--- app/src/main/java/aakanshaparmar/androiduploadimagetodb/MainActivity.java
+++ app/src/main/java/aakanshaparmar/androiduploadimagetodb/MainActivity.java
@@ -1,31 +1,45 @@
 package aakanshaparmar.androiduploadimagetodb;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button clickPhoto,OKButton;
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        this.imageView = (ImageView)this.findViewById(R.id.imageView1);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        clickPhoto = (Button) findViewById(R.id.cameraButton);
+        OKButton = (Button) findViewById(R.id.OKButton);
+        OKButton.setEnabled(false);
+
+        clickPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                Log.w("Test","Test");
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+
+        OKButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(this, UploadImg.class);
+                //startActivity(intent);
             }
         });
     }
@@ -50,5 +64,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+            OKButton.setEnabled(true);
+        }
     }
 }
